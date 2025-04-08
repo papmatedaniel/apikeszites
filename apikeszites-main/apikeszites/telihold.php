8beb441bb10c7ba0d50caaf7a7cf011b1dc46ad9
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2025-04-08 14:59:31 +0200

    Update telihold.php
    valotzasopk

--- apikeszites-main/apikeszites/telihold.php
+++ apikeszites-main/apikeszites/telihold.php
@@ -1,67 +1,98 @@
-<!-- Visszatérési érték: következő 3 holdtölte időpontja -->
-<!-- 1. Ha van paraméter, akkor a megadott időpontot követő 3 holdtölte időpontja -->
-<!-- 2. Ha nincs paraméter, akkor a mai dátumhoz képest számítja ki a kövi. 3 holdtölte dátumát -->
-<!-- 1. Lépés: írni egy függvényt, ami megkap egy dátumot, és visszaadja a kövi 3 holdtölte dátumát -->
 <?php
-// Évet, hónapot nem tudom percé alakítani a szökőévek miatt.  - Már tudok szökőévekkel számolni
-//Telihold volt pl. 2021. június 24-én, 20:40-kor. - Percé alakítjuk
-//Két azonos fázis közti időtartam (az ún. szinodikus keringésidő) 29.53058867 nap. - Percé alakítjuk
-//Megkapunk egy dátumot - Percé alakítjuk
-//A 2021. június 24 - percé alakított időpontjához elkezdjük hozzá adogatni a szonikus időt percekben.
-//Minden új hozzáadásnál visszaalkaítjuk év, hónap, nap, óra, perc dátummá, és megnézzük, hogy az a kért időpont után van e.7
-// Dátum percé alakítása
+function szokoeve($ev){
+    return ($ev % 4 == 0 && $ev % 100 != 0) || $ev % 400 == 0;
+}
+$honap_napok = array(
+    1 => 31,
+    2 => 28,  # Alapértelmezett február napok száma
+    3 => 31,
+    4 => 30,
+    5 => 31,
+    6 => 30,
+    7 => 31,
+    8 => 31,
+    9 => 30,
+    10 => 31,
+    11 => 30,
+    12 => 31
+); 
 
+function datumbolnap($datum){
+    /*Visszatérési érték: 0001.01.01 - paraméter(pl 2025.02.25) között eltelt napok száma */
+    $ev = intval(explode(".", $datum)[0]);
+    $honap = intval(explode(".", $datum)[1]);
+    $nap = intval(explode(".", $datum)[2]);
 
-
-function datumPerckonverter($datum){
-    $datum = explode(".", $datum);
-    $ev = (int)$datum[0];
-    $honap = (int)$datum[1];
-    $nap = (int)$datum[2];
-    $ora = (int)$datum[3];
-    $perc = (int)$datum[4];
-    
-    function szokoev($ev) {
-        return ($ev % 4 == 0 && $ev % 100 != 0) || ($ev % 400 == 0);
+    /* Évek konvertálása napokká */
+    $napokszama = 0;
+    for ($i=1; $i<$ev; $i++){
+        if (szokoeve($i)){
+            $napokszama += 366;
+        }
+        else{
+            $napokszama += 365;
+        }
     }
-    
-    $honap_napok = [
-        1 => 31,
-        2 => 28,
-        3 => 31,
-        4 => 30,
-        5 => 31,
-        6 => 30,
-        7 => 31,
-        8 => 31,
-        9 => 30,
-        10 => 31,
-        11 => 30,
-        12 => 31
-    ];
-    
-    if (szokoev($ev)) {
+
+    if (szokoev($ev)){
         $honap_napok[2] = 29;
     }
-    
-    $napokszama = 0;
-    for ($i = 1; $i < $ev; $i++) {
-        $napokszama += szokoev($i) ? 366 : 365;
-    }
-    
-    for ($i = 1; $i < $honap; $i++) {
+
+    for ($i=1; $i<$honap; $i++){
         $napokszama += $honap_napok[$i];
     }
-    
-    $napokszama += $nap;
-    
-    $percek = $napokszama * 24 * 60 + $ora * 60 + $perc;
-    
-    return $percek;
+
+    $napokszama += ($nap -1);
+    return $napokszama;
+
 }
 
-echo datumPerckonverter("2021.06.24.20.40");
+function napboldatum($napokszama){
+    $lista = 0; /*HIÁNYZIK*/
 
+    $evekszama = 1;
+    while ($napokszama >= 365){
+        $evekszama++;
+        if (szokoeve($evekszama)){
+            $napokszama -= 366;
+        }
+        else{
+            $napokszama -= 365;
+        }
+    }
+    if (szokoeve($evekszama)){
+        $napokszama++;
+        array_splice($lista, 59, 0, "2.29."); 
+    }
 
+    $honap_nap = $lista[$napokszama];
+    return (sprintf("%04d", $evekszama)) . ($honap_nap);
+
+}
+
+function datumbolperc($datum){
+    $ora = intval(explode(":", explode(".", $datum)[3])[0]);
+    $perc = intval(explode(":", explode(".", $datum)[3])[1]);
+    return $percekszama;
+}
+
+function percboloraperc($percekszama){
+    $ora = $percekszama / 60;
+    $perc = $percekszama % 60;
+    return [$ora, $perc];
+}
+
+function percboldatum($perc){
+    $napokszama = $perc / (60 * 24);
+    $percekszama = $perc % (60 * 24);
+    [$ora, $perc] = percboloraperc($percekszama);
+    return (napboldatum($napokszama)) . (sprintf("%02d", $ora)) . (sprintf("%02d", $perc));
+}
+
+function adottevteliholdjai($megadottidopont = "2025.04.08", $evek = 3){
+    $origo_ido = datumbolperc("2021.06.24.20:40");
+    (int)$szinodikus_ido_percben = (29.53058867 * 24 * 60);
+
+    $teliholdak_egy_evben = 365.25 / 29.53058867;
+}
 ?>
-
